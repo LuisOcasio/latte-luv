288eec05d956625d5d4b0b5fce0d2ce84b02a896
Luis Ocasio <ocasio.dev@gmail.com>
2020-01-04 01:03:12 -0500

    set social media links

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import ig from '../images/ig.png';
 import fb from '../images/fb.png';
@@ -8,15 +7,15 @@ import gmail from '../images/gmail.png';
 const SocialMedia = () => {
   return (
     <div className='social-media'>
-      <Link>
+      <a href='https://www.instagram.com/latteluvdrivethru'>
         <img src={ig} alt='ig icon' />
-      </Link>
-      <Link>
-        <img src={gmail} alt='gmail icon' />
-      </Link>
-      <Link>
-        <img src={fb} alt='fb icon' />
-      </Link>
+      </a>
+      <a href='https://www.facebook.com/latteluvdrivethru'>
+        <img src={fb} alt='ig icon' />
+      </a>
+      <a href='mailto: marielatteluv@gmail.com'>
+        <img src={gmail} alt='ig icon' />
+      </a>
     </div>
   );
 };

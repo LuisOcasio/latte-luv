dacef31e6a9fa79acd2749fffe724c899af584de
Luis Ocasio <ocasio.dev@gmail.com>
2020-01-03 21:20:33 -0500

    added images and styles

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import lattemenu from '../images/lattemenu.jpg';
+import menu from '../images/lattemenu.jpg';
+import sandwhich from '../images/latteluv-sandwhich.jpg';
+import lattemenu from '../images/latte&menu.jpg';
+import latte from '../images/caramel-latte.jpg';
 
 const Menu = () => {
   return (
     <div className='menu-page'>
-      <span role='img' aria-label='latte'>
-        ☕
-      </span>
+      <div className='menu'>
+        <img className='latteluv-menu' src={menu} alt='menu-pic' />
+      </div>
 
       <div className='menu-title'>
         The taste of a rich, smooth espresso drink, combined with steamed,
@@ -16,9 +19,18 @@ const Menu = () => {
 
       <hr />
 
-      <div className='menu'>
-        <img src={lattemenu} alt='menu-pic' />
+      <div className='bottom-section'>
+        <img src={lattemenu} alt='breakfast sandwhich' />
+        <img src={sandwhich} alt='breakfast sandwhich' />
+        <img src={latte} alt='breakfast sandwhich' />
       </div>
+
+      <footer>
+        "Here to serve you your coffee with "latte luv"
+        <span role='img' aria-label='latte'>
+          ☕
+        </span>
+      </footer>
     </div>
   );
 };
